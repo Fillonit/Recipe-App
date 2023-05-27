b21bbd625dcfc9958d0a6c9c6e345a70ca3b6b0e
Fillonit Ibishi <filloniti@gmail.com>
2023-05-27 21:53:56 +0200

    slight fixes

--- client/src/App.js
+++ client/src/App.js
@@ -16,27 +16,6 @@ import LogIn from './components/Register';
 import PublishRecipe from './components/Recipe/PublishRecipe';
 import Footer from './components/Footer';
 
-// const recipes = [
-//   {
-//     id: 1,
-//     title: 'Spaghetti with Tomato Sauce',
-//     image: 'https://www.giallozafferano.com/images/228-22832/spaghetti-with-tomato-sauce_1200x800.jpg',
-//     description: 'Classic spaghetti with tomato sauce and parmesan cheese.',
-//   },
-//   {
-//     id: 2,
-//     title: 'Garlic Butter Shrimp',
-//     image: 'https://www.jocooks.com/wp-content/uploads/2021/09/garlic-butter-shrimp-1-10.jpg',
-//     description: 'Juicy shrimp cooked in garlic butter and served with rice.',
-//   },
-//   {
-//     id: 3,
-//     title: 'Beef and Broccoli Stir-Fry',
-//     image: 'https://www.dinneratthezoo.com/wp-content/uploads/2017/10/beef-and-broccoli-stir-fry-14.jpg',
-//     description: 'Tender beef and crispy broccoli in a savory sauce.',
-//   },
-// ];
-
 function App() {
   return (
     <Router>
