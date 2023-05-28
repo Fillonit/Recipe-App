caa4d5a7d5b498e9ac5cb024ae4a4500b75566eb
Fillonit Ibishi <filloniti@gmail.com>
2023-05-28 15:25:11 +0200

    Register Update/Fix,

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Icon from '../images/icon.png';
 
 export default function Register() {
   const [username, setUsername] = useState("");
@@ -59,56 +60,83 @@ export default function Register() {
     setPassword(event.target.value);
   }
 
+  // return (
+  //   <div className={"mt-32 flex justify-center items-center h-screen"}>
+  //     <div className={"flex flex-col"}>
+  //       <div className={"flex flex-col items-center"}>
+  //         <h1 className={"text-4xl font-bold"}>Register</h1>
+  //         <input
+  //           className={"border-2 border-gray-400 rounded-lg p-2 m-2"}
+  //           type="text"
+  //           placeholder="Username"
+  //           value={username}
+  //           onChange={handleUsernameChange}
+  //         />
+  //         <input
+  //           className={"border-2 border-gray-400 rounded-lg p-2 m-2"}
+  //           type="password"
+  //           placeholder="Password"
+  //           value={password}
+  //           onChange={handlePasswordChange}
+  //         />
+  //         <button
+  //           className={"border-2 border-gray-400 rounded-lg p-2 m-2"}
+  //           onClick={handleRegistration}
+  //         >
+  //           Register
+  //         </button>
+
+  //         <h1 className={"text-4xl font-bold"}>Login</h1>
+  //         <input
+  //           className={"border-2 border-gray-400 rounded-lg p-2 m-2"}
+  //           type="text"
+  //           placeholder="Username"
+  //           value={username}
+  //           onChange={handleUsernameChange}
+  //         />
+  //         <input
+  //           className={"border-2 border-gray-400 rounded-lg p-2 m-2"}
+  //           type="password"
+  //           placeholder="Password"
+  //           value={password}
+  //           onChange={handlePasswordChange}
+  //         />
+  //         <button
+  //           className={"border-2 border-gray-400 rounded-lg p-2 m-2"}
+  //           onClick={handleLogin}
+  //         >
+  //           Login
+  //         </button>
+  //       </div>
+  //     </div>
+  //   </div>
+
+  // );
+
   return (
-    <div className={"mt-32 flex justify-center items-center h-screen"}>
-      <div className={"flex flex-col"}>
-        <div className={"flex flex-col items-center"}>
-          <h1 className={"text-4xl font-bold"}>Register</h1>
-          <input
-            className={"border-2 border-gray-400 rounded-lg p-2 m-2"}
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-          <input
-            className={"border-2 border-gray-400 rounded-lg p-2 m-2"}
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-          <button
-            className={"border-2 border-gray-400 rounded-lg p-2 m-2"}
-            onClick={handleRegistration}
-          >
-            Register
-          </button>
-
-          <h1 className={"text-4xl font-bold"}>Login</h1>
-          <input
-            className={"border-2 border-gray-400 rounded-lg p-2 m-2"}
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-          <input
-            className={"border-2 border-gray-400 rounded-lg p-2 m-2"}
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-          <button
-            className={"border-2 border-gray-400 rounded-lg p-2 m-2"}
-            onClick={handleLogin}
-          >
-            Login
-          </button>
+    <div class="flex h-screen bg-indigo-700">
+      <div class="w-full max-w-xs m-auto bg-indigo-100 rounded p-5">   
+      <header>
+        <img class="w-20 mx-auto mb-5" src={Icon} alt={'logo'} />
+      </header>   
+      <form>
+        <div>
+          <label class="block mb-2 text-indigo-500" for="username">Username</label>
+          <input class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300" type="text" name="username" onChange={handleUsernameChange}/>
         </div>
-      </div>
+        <div>
+          <label class="block mb-2 text-indigo-500" for="password">Password</label>
+          <input class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300" type="password" name="password" onChange={handlePasswordChange}/>
+        </div>
+        <div>          
+          <input class="w-full bg-indigo-700 hover:bg-pink-700 text-white font-bold py-2 px-4 mb-6 rounded" type="submit"/>
+        </div>       
+      </form>  
+      <footer>
+        <a class="text-indigo-700 hover:text-pink-700 text-sm float-left" href="/">Forgot Password?</a>
+        <a class="text-indigo-700 hover:text-pink-700 text-sm float-right" href="/" onClick={handleRegistration}>Create Account</a>
+      </footer>   
     </div>
-
+</div>
   );
 }
