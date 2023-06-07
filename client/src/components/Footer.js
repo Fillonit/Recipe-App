f8cc386025dd5db6074a47c6eb12e716e9e86d45
Fillonit Ibishi <filloniti@gmail.com>
2023-06-07 23:19:00 +0200

    Error and warning fixes

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,13 +10,13 @@ const Footer = () => {
             M
           </a>
           <div className="flex space-x-2">
-            <a href="#" className="text-gray-400 hover:text-white transition-colors duration-300">
+            <a href="/" className="text-gray-400 hover:text-white transition-colors duration-300">
               <FaFacebookF className="text-lg" />
             </a>
-            <a href="#" className="text-gray-400 hover:text-white transition-colors duration-300">
+            <a href="/" className="text-gray-400 hover:text-white transition-colors duration-300">
               <FaTwitter className="text-lg" />
             </a>
-            <a href="#" className="text-gray-400 hover:text-white transition-colors duration-300">
+            <a href="/" className="text-gray-400 hover:text-white transition-colors duration-300">
               <FaInstagram className="text-lg" />
             </a>
           </div>
