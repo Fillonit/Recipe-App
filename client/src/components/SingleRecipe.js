7e1c977fc21932149387743bab1b59a4ebca6435
Fillonit Ibishi <filloniti@gmail.com>
2023-06-14 07:09:41 +0200

    warn fix

--- client/src/components/SingleRecipe.js
+++ client/src/components/SingleRecipe.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ChefCard from './ChefCard';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock, faEye, faSave, faStar, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
