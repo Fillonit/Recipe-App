375f80194bee864111dbae6f80e1589193fd3514
Fillonit Ibishi <filloniti@gmail.com>
2023-06-14 12:12:50 +0200

    Ingredients fix for edit recipe

--- client/src/components/Recipe/Ingredients.js
+++ client/src/components/Recipe/Ingredients.js
@@ -1,18 +1,34 @@
 import Ingredient from "./Ingredient";
 import Steps from "./Steps";
 import "../../styles/recipeTags.css";
-export default function Ingredients({ setSteps, steps, ingredients, recipeIngredients, units, setRecipeIngredients }) {
-    const ingredientCount = Object.keys(recipeIngredients).length;
-    if (ingredientCount !== 0 || steps.length !== 0)
-        return (
-            <div className="bg-violet-200 self-start shadow-md rounded px-8 pt-6 pb-8 ml-20 mb-4 w-1/4 h-auto">
-                {ingredientCount !== 0 && <h3> Ingredients</h3>}
-                <div className="ingredientContainer">
-                    {Object.keys(recipeIngredients).map((key, index) => {
-                        return <Ingredient setIngredients={setRecipeIngredients} index={index} ingredient={{ ...ingredients[key], unit: units[recipeIngredients[key].unit].UnitName, amount: recipeIngredients[key].amount }} />
-                    })}
-                </div>
-                <Steps setSteps={setSteps} steps={steps} />
-            </div >
-        );
+
+export default function Ingredients({ recipe, setSteps, steps, ingredients, recipeIngredients, units, setRecipeIngredients }) {
+  const ingredientCount = Object.keys(recipeIngredients).length;
+  if (ingredientCount !== 0 || steps.length !== 0) {
+    return (
+      <div className="bg-violet-200 self-start shadow-md rounded px-8 pt-6 pb-8 ml-20 mb-4 w-1/4 h-auto">
+        {ingredientCount !== 0 && <h3> Ingredients</h3>}
+        <div className="ingredientContainer">
+          {Object.keys(recipeIngredients).map((key, index) => {
+            if (key === recipe) {
+              return (
+                <Ingredient
+                  setIngredients={setRecipeIngredients}
+                  index={index}
+                  ingredient={{
+                    ...ingredients[key],
+                    unit: units[recipeIngredients[key].unit].UnitName,
+                    amount: recipeIngredients[key].amount,
+                  }}
+                />
+              );
+            }
+            return null;
+          })}
+        </div>
+        <Steps setSteps={setSteps} steps={steps} />
+      </div>
+    );
+  }
+  return null;
 }
